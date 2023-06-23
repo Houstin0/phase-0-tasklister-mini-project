9d28e48e4ebe27b66d19459c23008918041f7830
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-06-23 15:12:28 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    addListItem(e)
+ })
 });
+
+function addListItem(e){
+ //console.log(e.target.new-task-description.value)
+  const taskItem=e.target['new-task-description'].value;
+ 
+  let listItem=document.createElement('li')
+
+  listItem.textContent=taskItem
+  document.querySelector('#tasks').append(listItem)
+   
+
+  let listItemDelete = document.createElement('Button')
+  listItemDelete.textContent = "Delete"
+    listItem.appendChild(listItemDelete)
+    listItemDelete.style.margin = '5px 10px'
+    listItemDelete.addEventListener("click", deleteTaskItem)
+
+   
+}
+ function deleteTaskItem(e){
+   e.target.parentNode.remove()
+ }
+ 
